0d19f28d7a654410e50a08f01bfa3baffd19d0fe
Maribel <maribel.torres.guevara@gmail.com>
2022-12-29 17:36:45 -0500

    Agregando la seccion technology

---
+++ src/app/technology/content-technology/content-technology.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-technology',
+  templateUrl: './content-technology.component.html',
+  styleUrls: ['./content-technology.component.css']
+})
+export class ContentTechnologyComponent {
+
+}
