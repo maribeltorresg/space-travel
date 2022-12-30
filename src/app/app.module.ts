91db83960f98d4de2ddf1429e55d6f1885fb2e62
Maribel <maribel.torres.guevara@gmail.com>
2022-12-30 10:28:34 -0500

    Agregando los estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrewModule } from './crew/crew.module';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CrewModule
-  ],
 import { TechnologyModule } from './technology/technology.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TechnologyModule],
   providers: [],
+  imports: [BrowserModule, AppRoutingModule, CrewModule, TechnologyModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
