b66220dd997dc5701ddc0a61603f4b43b3a6ba97
alesslu <alessluchata@gmail.com>
2022-12-30 01:03:07 -0500

    añadiendo destination

---
+++ src/app/destination/destino/destino.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destino',
+  templateUrl: './destino.component.html',
+  styleUrls: ['./destino.component.css']
+})
+export class DestinoComponent {
+
+}
