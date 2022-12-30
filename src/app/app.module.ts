c72f3b43a80501d9f698a17eeff331a22503af0b
Ricardo J. Castillo López <87798579+rjcastillolopez@users.noreply.github.com>
2022-12-29 19:39:49 -0500

    adding crew

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrewModule } from './crew/crew.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CrewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
