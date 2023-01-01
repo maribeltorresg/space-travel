71b654bb463654c896956458857daf0970281e32
Maribel <maribel.torres.guevara@gmail.com>
2023-01-01 15:11:09 -0500

    Agregando el navbar en cada seccion

--- src/app/crew/crew.module.ts
+++ src/app/crew/crew.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { MainComponent } from './main/main.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    CardComponent,
-    MainComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    CardComponent,
-    MainComponent
-  ]
+  declarations: [CardComponent, MainComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [CardComponent, MainComponent],
 })
-export class CrewModule { }
+export class CrewModule {}
